8e6c538cc955038b16648591be53faad49869f0a
Jordan Collier <jordanicollier@gmail.com>
2015-02-24 15:00:36 -0700

    add mount model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150224215843) do
+ActiveRecord::Schema.define(version: 20150224220019) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,14 @@ ActiveRecord::Schema.define(version: 20150224215843) do
     t.integer  "damage"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+  end
+
+  create_table "mounts", force: :cascade do |t|
+    t.string   "name"
+    t.string   "species"
+    t.boolean  "flying"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "parties", force: :cascade do |t|
