19534c25892f42f0add34e35ed9e8a763d91d8a5
Jordan Collier <jordanicollier@gmail.com>
2015-02-24 15:06:50 -0700

    add character class model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150224220019) do
+ActiveRecord::Schema.define(version: 20150224220503) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "character_classes", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "characters", force: :cascade do |t|
     t.string   "name"
