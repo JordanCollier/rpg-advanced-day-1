ec66a8f9d9ee2f65a66fe25c4b3e809bc85818d6
Jordan Collier <jordanicollier@gmail.com>
2015-02-24 15:10:56 -0700

    add special item model

---
+++ db/migrate/20150224220947_create_special_items.rb
@@ -0,0 +1,10 @@
+class CreateSpecialItems < ActiveRecord::Migration
+  def change
+    create_table :special_items do |t|
+      t.string :name
+      t.text :description
+
+      t.timestamps null: false
+    end
+  end
+end
