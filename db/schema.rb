ef9ca50ed184bf00af2ab6f63f57d98d3111630e
Jordan Collier <jordanicollier@gmail.com>
2015-02-24 15:08:37 -0700

    add an ability model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150224220503) do
+ActiveRecord::Schema.define(version: 20150224220812) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "abilities", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "mana_cost"
+    t.integer  "damage"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "character_classes", force: :cascade do |t|
     t.string   "name"
